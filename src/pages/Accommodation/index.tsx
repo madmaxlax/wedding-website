7c7402eb48a4d4382d50c6edd5f877ce338ba2f3
Max Struever <struever.max@bcgplatinion.com>
2022-07-07 10:54:38 -0400

    packages lock

--- src/pages/Accommodation/index.tsx
+++ src/pages/Accommodation/index.tsx
@@ -5,7 +5,7 @@ import { MapEmbed } from './MapEmbed';
 
 export const AccommodationPage = () => {
   return (
-    <>
+    <React.Fragment>
       <div id="fh5co-when-where" className="fh5co-section-gray">
         <div className="container">
           <div className="row">
@@ -13,8 +13,17 @@ export const AccommodationPage = () => {
               <h2>Accommodation</h2>
             </div>
           </div>
-          <div className="row row-bottom-padded-md">
-            <div className="col-md-12 text-center animate-box">
+          <div className="row row-bottom-padded-md" style={{ textAlign: 'center' }}>
+            <iframe
+              width="100%"
+              style={{ maxWidth: 850 }}
+              height={1000}
+              frameBorder={0}
+              marginHeight={0}
+              marginWidth={0}
+              src="https://docs.google.com/document/d/e/2PACX-1vS4K8_wYda0eYNpUrdjHEy0ZrCQ9LBEYMTq1X4YPeoo_Ez7u5Tu6p9YjgBLqPmu7ke5MwFVUo8VSQRY/pub?embedded=true"
+            ></iframe>
+            {/* <div className="col-md-12 text-center animate-box">
               <div className="wedding-events">
                 <div className="ceremony-bg" style={{ backgroundImage: 'url(images/skan-lake.jpg)' }}></div>
                 <div className="desc">
@@ -29,16 +38,16 @@ export const AccommodationPage = () => {
                   </p>
                   {/* <div style={{ height: 500, width: '100%' }}>
       <DataGrid rows={rows} columns={columns} />
-    </div> */}
+    </div> 
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
           <div className="row">
             <MapEmbed />
           </div>
         </div>
       </div>
-    </>
+    </React.Fragment>
   );
 };
--- src/pages/RSVP/index.tsx
+++ src/pages/RSVP/index.tsx
@@ -21,7 +21,7 @@ export const RSVP = () => {
                 <iframe
                   src="https://docs.google.com/forms/d/e/1FAIpQLSewZWhwn4ZYCP4ao0L7zdP-mkLn_U1lLcZqSvphsxpWEgCjqw/viewform?embedded=true"
                   width="100%"
-                  height={400}
+                  height={1000}
                   frameBorder={0}
                   marginHeight={0}
                   marginWidth={0}
